609e35db8106d9272a24a23ee0ff901f37d04f27
MIkhail <mmv-774@yandex.ru>
2022-03-24 17:08:14 +0500

    изменять состояние чекбокса при нажатии

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Checkbox = ({ id, caption }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <label htmlFor={`checkbox-${id}`} className='checkbox-pseudo'>
-      <input type='checkbox' id={`checkbox-${id}`} className='checkbox-pseudo__invisible' />
+    <label htmlFor={`checkbox-${id}`} className={`checkbox-pseudo ${isActive && 'checkbox-pseudo_active'}`}>
+      <input type='checkbox' id={`checkbox-${id}`} className='checkbox-pseudo__invisible' onClick={handleClick} />
       <span className='checkbox-pseudo__visible'>{caption}</span>
     </label>
   );
